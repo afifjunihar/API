a2c7aa84b46de7d25939628e3f2070037cb0bc38
mhfdSyauqi <68425293+mhfdSyauqi@users.noreply.github.com>
2021-11-10 10:26:20 +0700

    add client

---
+++ Client/wwwroot/js/chart.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    chart.render();
+});
+
+const dataSeries = [];
+const dataLabel = [];
+
+var dataProp = $.ajax({
+  type: 'GET',
+  url: 'https://localhost:5001/api/employees/chart',
+  success: function (data) {
+    $.each(data, function (index, value) {
+      dataSeries.push(value.count);
+      dataLabel.push(value.degree);
+		})
+	}
+})
+
+// ApexChart
+var options = {
+  chart: {
+    type: 'pie'
+  },
+  series: dataSeries,
+  labels: dataLabel
+}
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
